e1ab04cd71197ed0b5518dea2e9ea3b37a74360d
Melanie MacDonald <macdon6m@udml7.local>
2018-09-26 18:13:31 -0600

    Added ability to change label using text field; added an image view. MM

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,14 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
 
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
     }
-
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        // indicates whether system should respond to the press of the "Return" or "Done" key or not
+        // in this case, there's no reason not to respond, so it should always return true
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
