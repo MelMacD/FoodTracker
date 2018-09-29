5e31f8ae9316a09f01648f62c89f646090a3c3f4
Melanie MacDonald <macdon6m@udml4.cs.uregina.ca>
2018-09-28 18:44:15 -0600

    Created table entry view controller and added some photos. MM

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Melanie MacDonald on 2018-09-28.
+//  Copyright © 2018 Melanie MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
