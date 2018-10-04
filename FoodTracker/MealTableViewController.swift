95a058bc604ab3ae887f9b977f5bff27f870acb5
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-04 10:46:01 -0600

    Update MealTableViewController.swift
    Added comments to MealTableViewController.swift. MM

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -2,6 +2,9 @@
 //  MealTableViewController.swift
 //  FoodTracker
 //
+// Controls the Table View, which involves displaying all Meal objects from persistent storage and providing a method for editing or
+// selecting them
+//
 //  Created by Melanie MacDonald on 2018-10-01.
 //  Copyright © 2018 Melanie MacDonald. All rights reserved.
 //
@@ -12,9 +15,10 @@ import os.log
 class MealTableViewController: UITableViewController {
     
     //MARK: Properties
-    
+    // Handles the Meals as an array of Meal objects
     var meals = [Meal]()
 
+    // On loading the view, add an edit button and attempt to load any meals saved, otherwise loading sample data instead
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,14 +37,18 @@ class MealTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    // Sets the number of sections for the Table View, in this case there being only one
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
+    // Sets the number of rows in the section to the number of Meal objects in the meals array
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return meals.count
     }
 
+    // Sets each table cell to its appropriate values per Meal object in the meals array, using the IndexPath as an index for meals
+    // array as well to ensure the meals are displayed in order of storage (or in order defined as sample data)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Table view cells are reused and should be dequeued using a cell identifier
@@ -61,13 +69,16 @@ class MealTableViewController: UITableViewController {
     }
 
     // Override to support conditional editing of the table view.
+    // This turns on editing of the table view, which is used in this case to potentially deleted rows/Meal objects from the table
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
     
     // Override to support editing the table view.
+    // Only defined for deleting a table row, because there is a different process for adding rows defined in this program
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // Ensures the data storage is updated with the removal of a meal as well as updating the table to reflect this change
         if editingStyle == .delete {
             // Delete the row from the data source
             meals.remove(at: indexPath.row)
@@ -77,26 +88,13 @@ class MealTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // This code helps to distinguish between adding a new meal or editing an existing one before navigating to the Meal Detail View,
+    // by either setting the meal attribute of the mealDetailViewController to the currently selected meal that the user tapped on so
+    // that it can be displayed on the upcoming view, or not setting the meal attribute so the view knows to display the default data
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         
@@ -124,16 +122,18 @@ class MealTableViewController: UITableViewController {
     }
 
     //MARK: Actions
-    
+    // Executes on returning from the Meal Detail View, and either updates a meal if it is modified or adds a new row for a new meal
+    // and updates the persistent data storage
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
         if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
+            // if a row is selected, then the meal must have been modified, otherwise it must have been a new meal
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
                 // Update an existing meal
                 meals[selectedIndexPath.row] = meal
                 tableView.reloadRows(at: [selectedIndexPath], with: .none)
             }
             else {
-                // Add a new meal
+                // Add a new meal to the meals array and modify the table view to reflect this new row
                 let newIndexPath = IndexPath(row: meals.count, section: 0)
                 
                 meals.append(meal)
@@ -141,13 +141,13 @@ class MealTableViewController: UITableViewController {
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
             
-            //Save the meals
+            //Save any new or modified meals in data storage
             saveMeals()
         }
     }
     
     //MARK: Private Methods
-    
+    // If there are no meals saved, create sample meals for display instead so that the table view is never empty
     private func loadSampleMeals() {
         
         let photo1 = UIImage(named: "meal1")
@@ -169,6 +169,7 @@ class MealTableViewController: UITableViewController {
         meals += [meal1, meal2, meal3]
     }
     
+    // Save all meals into persistent storage, otherwise logging an error
     private func saveMeals() {
         let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
         
@@ -180,6 +181,7 @@ class MealTableViewController: UITableViewController {
         }
     }
     
+    // Retrieve any saved meals as an array of Meal objects
     private func loadMeals() -> [Meal]? {
         return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
     }
