011e25ffe9ae742cd556dfb65af3b6a0d46a6f3a
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-04 10:21:36 -0600

    Update MealTableViewCell.swift
    Added comments to MealTableViewCell.swift. MM

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -2,6 +2,8 @@
 //  MealTableViewCell.swift
 //  FoodTracker
 //
+// Provides a way to create and organize information within the Table View for each cell
+//
 //  Created by Melanie MacDonald on 2018-09-28.
 //  Copyright © 2018 Melanie MacDonald. All rights reserved.
 //
@@ -11,6 +13,7 @@ import UIKit
 class MealTableViewCell: UITableViewCell {
     
     //MARK: Properties
+    // Provides these outlets as a way to modify or read the information within the cells that is displayed to the user
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
@@ -20,6 +23,7 @@ class MealTableViewCell: UITableViewCell {
         // Initialization code
     }
 
+    // Controls whether a cell is seen as selected, which has certain behavior attached such as visually altering the cell when selected
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
