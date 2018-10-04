106b7df703ee811a58b075b286299d288ee1fc1c
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-04 10:18:19 -0600

    Update Meal.swift
    Added comments to Meal.swift file. MM

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -2,6 +2,9 @@
 //  Meal.swift
 //  FoodTracker
 //
+// The purpose of this file is to provide a class that defines a Meal, so Meal objects can be created, modified, displayed, and stored
+// with certain constraints given that dictate what is and isn't appropriate values for the Meal attributes
+//
 //  Created by Melanie MacDonald on 2018-09-28.
 //  Copyright © 2018 Melanie MacDonald. All rights reserved.
 //
@@ -9,6 +12,8 @@
 import UIKit
 import os.log
 
+// This class uses NSObject and NSCoding to facilitate persistent storage of Meal objects by providing a way to encode and decode the
+// Meal attributes to and from storage
 class Meal: NSObject, NSCoding {
     
     //MARK: Properties
@@ -18,12 +23,12 @@ class Meal: NSObject, NSCoding {
     var rating: Int
     
     //MARK: Archiving Paths
-    
+    // Dictate where and how the persistent storage is kept
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
     
     //MARK: Types
-    
+    // Provides key to keep track of values saved in persistent storage, used when encoding and decoding to process the correct attributes
     struct PropertyKey {
         static let name = "name"
         static let photo = "photo"
@@ -31,7 +36,7 @@ class Meal: NSObject, NSCoding {
     }
     
     //MARK: Initialization
-    
+    // Provides constraints for the Meal attributes where appropriate
     init?(name: String, photo: UIImage?, rating: Int) {
         // The name must not be empty
         guard !name.isEmpty else {
@@ -50,13 +55,14 @@ class Meal: NSObject, NSCoding {
     }
     
     //MARK: NSCoding
-    
+    // Encodes attributes into the proper format for storage
     func encode(with aCoder: NSCoder) {
         aCoder.encode(name, forKey: PropertyKey.name)
         aCoder.encode(photo, forKey: PropertyKey.photo)
         aCoder.encode(rating, forKey: PropertyKey.rating)
     }
     
+    // Decodes attributes and casts them into their respective types if present before saving them back into the Meal attributes
     required convenience init?(coder aDecoder: NSCoder) {
         
         // The name is required. If we cannot decode a name string, the initializer should fail
